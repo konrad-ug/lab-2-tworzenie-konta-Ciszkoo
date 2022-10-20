d14034a5dab1a7a150960a94097330ed58bb2522
Ciszkoo <pawel.olszko.p@gmail.com>
2022-10-20 17:28:26 +0200

    testy dla peseli o niepoprawnej dlugosci

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -4,17 +4,29 @@ from ..Konto import Konto
 
 
 class TestCreateBankAccount(unittest.TestCase):
-    def test_tworzenie_konta(self):
-        imie = "Dariusz"
-        nazwisko = "Januszewski"
-        pesel = "12345678912"
-        pierwsze_konto = Konto(imie, nazwisko, pesel)
+    imie = "Dariusz"
+    nazwisko = "Januszewski"
 
-        self.assertEqual(pierwsze_konto.imie, imie, "Imie nie zostało zapisane!")
+    def test_tworzenie_konta(self):
+        pierwsze_konto = Konto(self.imie, self.nazwisko, pesel)
+        pesel = "12345678912"
+
+        self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
         self.assertEqual(
-            pierwsze_konto.nazwisko, nazwisko, "Nazwisko nie zostało zapisane!"
+            pierwsze_konto.nazwisko, self.nazwisko, "Nazwisko nie zostało zapisane!"
         )
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+
+        # tutaj proszę dodawać nowe testy
+
         self.assertEqual(pierwsze_konto.pesel, pesel, "PESEL nie zostal zapisany!")
 
-    # tutaj proszę dodawać nowe testy
+    def test_tworzenie_konta_zbyt_krotki_pesel(self):
+        pesel = "123"
+        konto = Konto(self.imie, self.nazwisko, pesel)
+        self.assertEqual(konto.pesel, "Niepoprawny PESEL!", "Podany PESEL jest zbyt krotki!")
+
+    def test_tworzenie_konta_zbyt_dlugi_pesel(self):
+        pesel = "1241415161666727"
+        konto = Konto(self.imie, self.nazwisko, pesel)
+        self.assertEqual(konto.pesel, "Niepoprawny PESEL!", "Podany PESEL jest zbyt dlugi!")
