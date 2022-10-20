1cd58312258c0c08ead94f487472c9dfb37dc314
Ciszkoo <pawel.olszko.p@gmail.com>
2022-10-20 23:39:43 +0200

    klasa konto zaaktualizowana o kod bonusowy

--- app/Konto.py
+++ app/Konto.py
@@ -1,10 +1,16 @@
+import re
+
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, imie, nazwisko, pesel, promo_kod = None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = 0
 
         if len(pesel) == 11:
             self.pesel = pesel
         else:
             self.pesel = "Niepoprawny PESEL!"
+
+        if promo_kod == None or re.fullmatch("^PROM-.{3}$", promo_kod) == None:
+            self.saldo = 0
+        else:
+            self.saldo = 50
