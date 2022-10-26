f38527a8a87c82236ad23796ddd3fad99a4cf115
Ciszkoo <pawel.olszko.p@gmail.com>
2022-10-26 14:48:28 +0200

    implementacja sprawdzania wieku klienta w przypadku korzystania z promocji

--- app/Konto.py
+++ app/Konto.py
@@ -1,7 +1,8 @@
 import re
 
+
 class Konto:
-    def __init__(self, imie, nazwisko, pesel, promo_kod = None):
+    def __init__(self, imie, nazwisko, pesel, promo_kod=None):
         self.imie = imie
         self.nazwisko = nazwisko
 
@@ -10,7 +11,16 @@ class Konto:
         else:
             self.pesel = "Niepoprawny PESEL!"
 
+        _rok_suffix = int(pesel[0:2])
+        rok_prefix = int(pesel[2:4])
+
         if promo_kod == None or re.fullmatch("^PROM-.{3}$", promo_kod) == None:
+            self.saldo = 0
+        elif (
+            self.pesel == "Niepoprawny PESEL!"
+            or rok_prefix > 80
+            or (rok_prefix <= 12 and _rok_suffix < 61)
+        ):
             self.saldo = 0
         else:
             self.saldo = 50
