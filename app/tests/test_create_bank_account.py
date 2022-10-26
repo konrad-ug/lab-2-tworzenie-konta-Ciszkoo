c62bfc6d87b6209b134dfd9004cb1b37b5ea2493
Ciszkoo <pawel.olszko.p@gmail.com>
2022-10-26 14:42:46 +0200

    testy sprawdzania wieku przy promocji

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -6,8 +6,9 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
     imie = "Dariusz"
     nazwisko = "Januszewski"
-    pesel = "12345678912"
+    pesel = "12325678912"  # 12.2012r.
     komunikat_zly_pesel = "Niepoprawny PESEL!"
+    poprawny_kod_promocyjny = "PROM-XYZ"
 
     def test_tworzenie_konta(self):
         pierwsze_konto = Konto(self.imie, self.nazwisko, self.pesel)
@@ -37,9 +38,10 @@ class TestCreateBankAccount(unittest.TestCase):
         )
 
     def test_konto_kod_prmocyjny(self):
-        kod_promocyjny = "PROM-XYZ"
         bonus_srodki = 50
-        konto = Konto(self.imie, self.nazwisko, self.pesel, kod_promocyjny)
+        konto = Konto(
+            self.imie, self.nazwisko, self.pesel, self.poprawny_kod_promocyjny
+        )
 
         self.assertEqual(
             konto.saldo, bonus_srodki, "Bonusowe srodki nie zostaly dodane!"
@@ -52,3 +54,15 @@ class TestCreateBankAccount(unittest.TestCase):
         self.assertEqual(
             konto.saldo, 0, "Bonusowe srodki zostaly dodane mimo zlego kodu!"
         )
+
+    def test_urodzony_przed_1900(self):
+        pesel = "99811234567"   # 01.1899r.
+        konto = Konto(self.imie, self.nazwisko, pesel, self.poprawny_kod_promocyjny)
+
+        self.assertEqual(konto.saldo, 0, "Bonusowe srodki zostaly przyznane seniorowi!")
+
+    def test_urodzony_przed_1961(self):
+        pesel = "60011234567"   # 01.1960r.
+        konto = Konto(self.imie, self.nazwisko, pesel, self.poprawny_kod_promocyjny)
+
+        self.assertEqual(konto.saldo, 0, "Bonusowe srodki zostaly przyznane seniorowi!")
