017bf1dbdb62976b0f8505ad9a0348a10cdf9ed7
Ciszkoo <pawel.olszko.p@gmail.com>
2022-10-28 17:39:31 +0200

    implementacja konta firmowego

---
+++ app/CompanyAccount.py
@@ -0,0 +1,27 @@
+from .Konto import Konto
+
+
+def nip_processing(nip):
+    if len(nip) == 10:
+        return nip
+    else:
+        return "Niepoprawny NIP!"
+
+
+class CompanyAccount(Konto):
+    def __init__(self, company_name, nip):
+        self.company_name = company_name
+        self.nip = nip
+        self._balance = 0
+
+    @property
+    def nip(self):
+        return self._nip
+
+    @nip.setter
+    def nip(self, nip):
+        self._nip = nip_processing(nip)
+
+    @property
+    def balance(self):
+        return self._balance
