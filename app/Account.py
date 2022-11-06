b38543eeadacc2f739c3fb404d1a1d28fc29cbef
Ciszkoo <pawel.olszko.p@gmail.com>
2022-11-06 10:50:22 +0100

    implementacja historii przelewow

--- app/Account.py
+++ app/Account.py
@@ -37,20 +37,25 @@ def initial_balance(pesel, promo_code):
 
 
 class Account:
+    express_transfer_commission = 1
+
     def __init__(self, name, surname, pesel, promo_code=None):
         self.name = name
         self.surname = surname
         self.pesel = pesel_processing(pesel)
         self.balance = initial_balance(self.pesel, promo_code)
-        self.express_transfer_commission = 1
+        self.transfer_history = []
 
     def incoming_transfer(self, amount):
         self.balance += amount
+        self.transfer_history.append(amount)
 
     def outgoing_transfer(self, amount):
         if self.balance > amount:
             self.balance -= amount
+            self.transfer_history.append(-amount)
 
     def express_transfer(self, amount):
         if amount <= self.balance:
             self.balance = self.balance - amount - self.express_transfer_commission
+            self.transfer_history.extend([-amount, -self.express_transfer_commission])
