a076418490d5b6be9e2c766e70a8822a86cfff04
Ciszkoo <pawel.olszko.p@gmail.com>
2022-11-17 16:39:47 +0100

    implementacja

--- app/BusinessAccount.py
+++ app/BusinessAccount.py
@@ -16,3 +16,9 @@ class BusinessAccount(Account):
         self.nip = nip_processing(nip)
         self.balance = 0
         self.transfer_history = []
+
+    def take_loan(self, amount):
+        if self.balance >= amount * 2 and len(list(filter(lambda x: x == -1775, self.transfer_history))) > 0:
+            self.balance += amount
+            return True
+        return False
