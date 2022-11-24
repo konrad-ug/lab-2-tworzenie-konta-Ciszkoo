af861401d5dd65020fe9b56c5d4472f853cb06fe
Ciszkoo <pawel.olszko.p@gmail.com>
2022-11-24 18:29:45 +0100

    zaktualizowane nazwy testow o kolejnosc

--- app/tests/test_accounts_register.py
+++ app/tests/test_accounts_register.py
@@ -9,25 +9,25 @@ class TestAccountsRegister(unittest.TestCase):
     surname = "Januszewski"
     pesel = "12325678912"
 
-    def test_adding_first_account(self):
+    def test_1_adding_first_account(self):
         account = Account(self.name, self.surname, self.pesel)
         AccountsRegister.add_account(account)
         self.assertEqual(AccountsRegister.number_of_accounts(), 1)
 
-    def test_adding_second_account(self):
+    def test_2_adding_second_account(self):
         account = Account(self.name, self.surname, "12325768912")
         AccountsRegister.add_account(account)
         self.assertEqual(AccountsRegister.number_of_accounts(), 2)
 
-    def test_adding_third_account(self):
+    def test_3_adding_third_account(self):
         account = Account(self.name, self.surname, self.pesel)
         AccountsRegister.add_account(account)
         self.assertEqual(AccountsRegister.number_of_accounts(), 3)
 
-    def test_searching_for_existing_account(self):
+    def test_4_searching_for_existing_account(self):
         account = AccountsRegister.find_account("12325768912")
         self.assertEqual(account.pesel, "12325768912")
 
-    def test_searching_for_non_existing_account(self):
+    def test_5_searching_for_non_existing_account(self):
         account = AccountsRegister.find_account("02325768910")
         self.assertEqual(account, None)
