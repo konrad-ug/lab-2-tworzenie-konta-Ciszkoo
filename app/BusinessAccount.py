a17b45c4b92ccbebdf76e6861d87c3f50381dd71
Ciszkoo <pawel.olszko.p@gmail.com>
2022-12-08 21:48:23 +0100

    implmentacja walidacji nipu przez api

--- app/BusinessAccount.py
+++ app/BusinessAccount.py
@@ -1,9 +1,15 @@
+from datetime import date
+import requests
+import os
+
 from .Account import Account
 
 
-def nip_processing(nip):
+def nip_processing(nip: str) -> str:
     if len(nip) == 10:
-        return nip
+        if BusinessAccount.is_nip_valid(nip):
+            return nip
+        return "Pranie!"
     else:
         return "Niepoprawny NIP!"
 
@@ -11,17 +17,27 @@ def nip_processing(nip):
 class BusinessAccount(Account):
     express_transfer_commission = 5
 
-    def __init__(self, company_name, nip):
+    def __init__(self, company_name: str, nip: str):
         self.company_name = company_name
         self.nip = nip_processing(nip)
         self.balance = 0
         self.transfer_history = []
 
-    def take_loan(self, amount):
+    def take_loan(self, amount: int) -> bool:
         if (
             self.balance >= amount * 2
             and len(list(filter(lambda x: x == -1775, self.transfer_history))) > 0
         ):
             self.balance += amount
+            return True
+        return False
+
+    @staticmethod
+    def is_nip_valid(nip: str) -> bool:
+        today = date.today()
+        url = os.environ.get('BANK_APP_MF_URL')
+        req = requests.get(
+            f'{url}{nip}?date={today}')
+        if req.status_code == 200:
             return True
         return False
